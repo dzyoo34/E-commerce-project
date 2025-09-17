65c9b546ead9784902e6b0d3389af3a09ed08e6b
dzyoo34 <165839007+dzyoo34@users.noreply.github.com>
2025-09-17 17:00:36 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,8 @@ import { Geist, Geist_Mono, Urbanist } from "next/font/google";
 import "./globals.css";
 import Footer from "@/components/footer";
 import Navbar from "@/components/navbar";
+import ModalProvider from "@/providers/modal-provider";
+import ToastProvider from "@/providers/toast-provider";
 
 const font = Urbanist({ subsets: ['latin']})
 const geistSans = Geist({
@@ -30,6 +32,8 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} ${font.className} antialiased`}
       >
+        <ModalProvider />
+        <ToastProvider />
         <Navbar />
         {children}
         <Footer />
