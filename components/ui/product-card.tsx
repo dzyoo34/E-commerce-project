947c9276ce8cfbc9355dd7570e7a9ca612eed748
dzyoo34 <165839007+dzyoo34@users.noreply.github.com>
2025-09-17 16:59:20 +0300

    Add files via upload

--- components/ui/product-card.tsx
+++ components/ui/product-card.tsx
@@ -6,6 +6,9 @@ import IconButton from "@/components/ui/icon-button";
 import { Expand, ShoppingCart } from "lucide-react";
 import Currency from "@/components/ui/currency";
 import { useRouter } from "next/navigation";
+import { MouseEventHandler } from "react";
+import usePreviewModal from "@/hooks/use-preview-modal";
+import useCart from "@/hooks/use-cart";
 
 interface ProductCard {
     data: Product;
@@ -13,10 +16,25 @@ interface ProductCard {
 const ProductCard: React.FC<ProductCard> = ({
     data
 }) => {
+    const cart = useCart();
+    const previewModal = usePreviewModal();
     const router = useRouter();
     const handleClick = () => {
         router.push(`/product/${data?.id}`);
     }
+
+    const onPreview: MouseEventHandler<HTMLButtonElement> = (event) => {
+        event.stopPropagation();
+
+        previewModal.onOpen(data);
+    }
+    const onAddToCart: MouseEventHandler<HTMLButtonElement> = (event) => {
+        event.stopPropagation();
+
+        cart.addItem(data);
+    }
+
+   
     return (
         <div  onClick={handleClick} className="bg-white group cursor-pointer rounded-xl border p-3 space-y-4">
             {/* Фото и действия */}
@@ -30,12 +48,12 @@ const ProductCard: React.FC<ProductCard> = ({
         <div className="opacity-0 group-hover:opacity-100 transition absolute w-full px-6 bottom-5">
             <div className="flex gap-x-6 justify-center">
             <IconButton 
-            onClick={() => {}}
+            onClick={onPreview}
             icon={<Expand size={20} className="text-gray-600"/>}
             className="group-hover:scale-110"
             />
             <IconButton 
-            onClick={() => {}}
+            onClick={onAddToCart}
             icon={<ShoppingCart size={20} className="text-gray-600"/>}
             className="group-hover:scale-110"
             />
